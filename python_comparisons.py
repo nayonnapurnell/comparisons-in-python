74fe6bdfee7cfee2d274964e13e1f7548bd69471
“Nayonna <Nayonna Purnell>
2020-03-06 00:36:42 -0500

    Completed the Python Comparisons Challenge

--- python_comparisons.py
+++ python_comparisons.py
@@ -1,12 +1,18 @@
+# TODO: Print out the User's name and the number entered,
+# making sure the two statements are on separate lines of output.
+
 name = input("Please enter your name: ")
 print(f"Hello, {name}.")
 number = int(input("Please enter a number: "))
 print(f"You've entered {number}.")
 
-# TODO: Print out the User's name and the number entered,
-# making sure the two statements are on separate lines of output.
 
+# TODO: Define variables for is_fizz and is_buzz that stores 
+# a Boolean value of the condition. Remember that the modulo operator, %, 
+# can be used to check if there is a remainder.
 
+is_fizz = number%3 == 0
+is_buzz = number%5 == 0
 
 # TODO: Compare the number the user gave with the different
 # FizzBuzz conditions. 
@@ -17,14 +23,17 @@ print(f"You've entered {number}.")
 # Otherwise, print "is neither a fizzy or a buzzy number."
 # *********************
 
+if is_fizz and is_buzz:
+    print(f"{number} is a FizzBuzz number.")
+elif is_fizz: 
+    print(f"{number} is a Fizz number.")
+elif is_buzz:
+    print(f"{number} is a Buzz number.")
+else:
+    print(f"{number} is neither a fizzy or a buzzy number.")
 
-# TODO: Define variables for is_fizz and is_buzz that stores 
-# a Boolean value of the condition. Remember that the modulo operator, %, 
-# can be used to check if there is a remainder.
 
 
-# Using the variables, check the condition of the value, and print the necessary
-# string
 
 
 
